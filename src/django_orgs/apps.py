27379bca578a75cf6dab27a9e1cb231e326bf4f3
Justin Mitchel <jmitchel3@gmail.com>
2025-01-14 14:03:24 -0700

    Foundational tests and models

--- src/django_orgs/apps.py
+++ src/django_orgs/apps.py
@@ -4,4 +4,10 @@ from django.apps import AppConfig
 class DjangoOrgsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_orgs"
-    verbose_name = "Django Orgs"
+    verbose_name = "Django Organizations"
+
+    def ready(self):
+        try:
+            import django_orgs.signals  # noqa
+        except ImportError:
+            pass
